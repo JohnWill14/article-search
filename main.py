cda657e5d94285fe6830ea2e704ba88db71e0dc3
John William Vicente <johnwill.v.2017@gmail.com>
2024-03-02 22:54:52 -0300

    add objective, method, problem

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 from nltk.stem import WordNetLemmatizer
+from nltk.collocations import BigramCollocationFinder
 
 import re
 import string
 
+import json
 
 def readPdf(path):
     reader = PdfReader(path)
@@ -78,69 +80,171 @@ def extractReferences(text):
     index = refrences.rfind(".")
     return refrences[0:index + 1]
 
+def extractAbstract(text):
+    find_abstract = text.find("Abstract")
+    find_keywords = text.find("I.")
+    return text[find_abstract:find_keywords]
+
+def extractIntro(text):
+    find_abstract = text.find("I.")
+    find_keywords = text.find("II.")
+    return text[find_abstract:find_keywords]
+def extractObjectiveByTerms(text):
+    word_keys =[
+        'this work',
+        'paper describe',
+        'This paper explain',
+        'This article explain',
+        "This paper describe",
+        'article describe',
+        'This work explain',
+        'work describe',
+        'This work propose',
+        'This paper propose',
+        'This article propose',
+        'This study',
+        'In this paper'
+    ]
+    word_keys = [w.lower() for w in word_keys]
+    sent_text = nltk.sent_tokenize(text.lower())
+    for sentence in sent_text:
+        s = sentence.replace('\n', "")
+        if any(substring in s for substring in word_keys):
+            return s
+    return "not found"
+
+def extractObjective(text):
+    abstract = extractAbstract(text)
+    objective = extractObjectiveByTerms(abstract).replace("abstract", "")
+
+    if objective == "not found":
+        intro = extractIntro(text)
+        return  extractObjectiveByTerms(intro).replace("abstract", "")
+
+    return objective
+
+def extractProblemByTerms(text):
+    word_keys =[
+        'this issue',
+        'this problem',
+        'issue',
+        'problem',
+        'Challenge',
+        'Dilemma',
+        'Impasse',
+        'Difficulty',
+        'Barrier',
+        'Hurdle',
+        'Conundrum'
+    ]
+    word_keys = [w.lower() for w in word_keys]
+    sent_text = nltk.sent_tokenize(text.lower())
+    for sentence in sent_text:
+        s = sentence.replace('\n', "")
+        if any(substring in s for substring in word_keys):
+            return s
+    return "not found"
+
+def extractProblem(text):
+    abstract = extractAbstract(text)
+    problem = extractProblemByTerms(abstract).replace("abstract", "")
+
+    if problem == "not found":
+        intro = extractIntro(text)
+        return  extractProblemByTerms(intro).replace("abstract", "")
+
+    return problem
+
+
+def extractMethodologyByTerms(text):
+    word_keys =[
+        'Based on',
+        'approach is'
+        'method',
+        'methodology',
+        'interviews',
+        'survey',
+        'content',
+        'analysis'
+    ]
+    word_keys = [w.lower() for w in word_keys]
+    sent_text = nltk.sent_tokenize(text.lower())
+    for sentence in sent_text:
+        s = sentence.replace('\n', "")
+        if any(substring in s for substring in word_keys):
+            return s
+    return "not found"
+
+def extractMethodology(text):
+    return  extractMethodologyByTerms(text)
+
+def article(title, most_common, objective, problem, method, references):
+    return {"title": title, "most_common": most_common, "objective": objective, "problem": problem, "method": method,
+     "references": references}
+def perform(path):
+    print(path)
+
+    # info
+    title = extractInfoFromPdf(path)
+    print(title)
+    # read
+    text = readPdf(path)
+    references = extractReferences(text)  # import
+    text = text.replace(references, "")
+
+    # token
+    tokens = tokenizeText(text)
+
+    # remove stop words
+    words_really_importants = removeStopWords(tokens)
+
+    # lemmatize
+    lemma = lemmatize(words_really_importants)
+
+    # stemming
+    ws = stemming(lemma)
+
+    # freq
+    freq = extractFreq(ws)
+
+    most_common10 = freq.most_common(20)  # import
+
+    # bigrama
+    bigram_measures = nltk.collocations.BigramAssocMeasures()
+    finder_bigram = BigramCollocationFinder.from_words(ws)
+
+    # print(finder_bigram.nbest(bigram_measures.pmi, 10))
+
+    # objective
+
+    objective = extractObjective(text)
+
+    # problem
+
+    problem = extractProblem(text)
+
+    # method
+    method = extractMethodologyByTerms(text)
+
+    return article(title, most_common10,objective,problem, method, references)
 
 if __name__ == '__main__':
     nltk.download('stopwords')
 
     pathDir = "/home/johnwill14/PycharmProjects/pythonProject/papers/Artigos IEEE - Forense"
     listdir = listdir(pathDir)
-    path = pathDir + os.sep + listdir[3]
-
-    # # read
-    # text = readPdf(path)
-    # references = extractReferences(text)
-    # text = text.replace(references, "")
-    #
-    # # token
-    # tokens = tokenizeText(text)
-    #
-    # # remove stop words
-    # words_really_importants = removeStopWords(tokens)
-    #
-    # # lemmatize
-    # lemma = lemmatize(words_really_importants)
-    #
-    # # stemming
-    # ws = stemming(lemma)
-    #
-    # # freq
-    # freq = extractFreq(ws)
-    #
-    # most_common10 = freq.most_common(10)
-    #
-    # for key in most_common10:
-    #     print(key)
 
     cont = 1
+    infos = []
     for filename in listdir:
 
         if filename.endswith(".pdf"):
             path = pathDir + os.sep + filename
 
-            # read
-            text = readPdf(path)
-            references = extractReferences(text)
-            text = text.replace(references, "")
+            l = perform(path)
+            output = json.dumps(l)
+            infos.append(output)
+    FILE_PATH = './data.txt'
 
-            # token
-            tokens = tokenizeText(text)
-
-            # remove stop words
-            words_really_importants = removeStopWords(tokens)
-
-            # lemmatize
-            lemma = lemmatize(words_really_importants)
-
-            # stemming
-            ws = stemming(lemma)
-
-            # freq
-            freq = extractFreq(ws)
-
-            most_common10 = freq.most_common(10)
-
-            print("PDF: ", cont)
-            print(extractInfoFromPdf(path))
-            cont += 1
-            for key in most_common10:
-                print(key)
+    with open(FILE_PATH, 'w') as output_file:
+            json.dump(infos, output_file, indent=2)
